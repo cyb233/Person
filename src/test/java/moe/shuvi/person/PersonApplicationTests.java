4eb1f3a17b0ead9b59f97cbaed8bb6e7b34a91d5
cyb233 <1252237203@qq.com>
2022-05-03 19:22:03 +0800

    Initial commit

---
+++ src/test/java/moe/shuvi/person/PersonApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.shuvi.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
